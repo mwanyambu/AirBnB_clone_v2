332b7df8e948353278aa2193266406953a1aebe2
mwanyambu <jmwanyambu@gmail.com>
2023-12-11 08:30:12 -0800

    setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,9 +7,15 @@ then
 fi
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared
-echo " Test file for nginx configuration." > /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Best School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 
-sudo sed -i "38i\\tlocation /hnbn_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+sudo sed -i "38i\\\tlocation /hnbn_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 sudo service nginx restart
