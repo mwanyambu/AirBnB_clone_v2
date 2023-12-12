fd1fd7ec1e278f8c81d250945d7a1d257923fd15
mwanyambu <jmwanyambu@gmail.com>
2023-12-12 05:04:27 -0800

    deployy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -29,7 +29,7 @@ def do_deploy(archive_path):
         run("sudo mv {}web_static/* {}".format(dir_path, dir_path))
         run("rm -rf {}{}/web_static".format(dir_path, dir_name))
         run("rm -rf /data/web_static/current")
-        run("ln -s {}{}/ /data/web_static/current".format(dir_path, dir_name))
+        run("ln -s {} /data/web_static/current".format(dir_path))
         return True
     except Exception:
         return False
