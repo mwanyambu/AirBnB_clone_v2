c8fdbd6a87af0b91fb3cb85c8861f6039237ec26
mwanyambu <jmwanyambu@gmail.com>
2023-12-29 04:08:54 -0800

    number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+"""script starts a flask web application"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """return hello HBNB"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns hbnb"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def ctext(text):
+    """returns C text"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pytext(text='is cool'):
+    """returns python text"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int n>', strict_slashes=False)
+def num(n):
+    """returns n is an integer"""
+    return "{:d} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
