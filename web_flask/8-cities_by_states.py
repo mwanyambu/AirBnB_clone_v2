fa0ea3a59f6ef20412c65f7d7ccaef6a2c4b3377
mwanyambu <jmwanyambu@gmail.com>
2023-12-30 10:49:42 -0800

    city by state

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""start falsk web app"""
+
+from flask import Flask, render_template
+from models import storage
+from models import *
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def state_cities():
+    """displays states with their cities"""
+    states = storage.all(State).values()
+    return render_template('cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """removes current session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
