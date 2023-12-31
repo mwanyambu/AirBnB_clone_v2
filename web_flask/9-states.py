9c3b1ecdeac93e1f985db89e8b693b3d31efa184
mwanyambu <jmwanyambu@gmail.com>
2023-12-31 12:08:47 -0800

    statess

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -15,20 +15,32 @@ def teardown(exception):
     storage.close()
 
 
-@app.route('/states', strict_slashes=False)
+"""@app.route('/states', strict_slashes=False)
 def state():
     """displays states html"""
     states = storage.all(State)
-    return render_template('9-states.html', states=states)
+    return render_template('9-states.html', states=states)"""
 
-
-@app.route('/states/<id>', strict_slashes=False)
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<state_id>', strict_slashes=False)
+def city_states(state_id=None):
+    """displays states and their cities"""
+    states = storage.all("State").values()
+    if state_id:
+        state_key = "State.{}".format(state_id)
+        state = storage.get(State, state_key)
+        if state:
+            cities = sorted(state.cities, key=lambda city: city.name)
+            return render_template('9-states.html', state=state, cities=cities)
+        return render_template('9-states.html', not_found=True)
+    return render_template('9-states.html', states=sorted(states, key=lambda state: state.name))
+"""@app.route('/states/<id>', strict_slashes=False)
 def states(id):
     """shows states"""
     for state in storage.all(State).values():
         if state.id == id:
             return render_template('9-states.html', state=state)
-    return render_template('9-states.html')
+    return render_template('9-states.html')"""
 
 
 if __name__ == "__main__":
